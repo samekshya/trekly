c3c1e31b71bb13995796bfde25c1007f52616fc0
Samekshya <samikshyabaniya003@gmail.com>
2026-01-01 19:49:02 +0545

    routes in app.dart

---
+++ lib/app/app.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+// screens
+import '../screens/splash_screen.dart';
+import '../screens/onboarding_screen.dart';
+import '../screens/login_screen.dart';
+import '../screens/register_screen.dart';
+import '../screens/bottom_screen/bottomnavigation_screen.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/splash',
+      routes: {
+        '/splash': (_) => const SplashScreen(),
+        '/onboarding': (_) => const OnboardingScreen(),
+        '/login': (_) => const LoginScreen(),
+        '/register': (_) => const RegisterScreen(),
+        '/home': (_) => const BottomNavigationScreen(),
+      },
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,48 @@
 import 'package:flutter/material.dart';
-import 'screens/splash_screen.dart';
-import 'screens/onboarding_screen.dart';
-import 'screens/login_screen.dart';
-import 'screens/register_screen.dart';
-import 'screens/home_screen.dart';
-import 'screens/bottom_screen/bottomnavigation_screen.dart';
 import 'app/app.dart';
 import 'core/services/hive_services.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await HiveService.init();
+  runApp(const App());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+// import 'package:flutter/material.dart';
+// import 'screens/splash_screen.dart';
+// import 'screens/onboarding_screen.dart';
+// import 'screens/login_screen.dart';
+// import 'screens/register_screen.dart';
+// import 'screens/home_screen.dart';
+// import 'screens/bottom_screen/bottomnavigation_screen.dart';
+// import 'app/app.dart';
+// import 'core/services/hive_services.dart';
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      initialRoute: '/splash',
-      routes: {
-        '/splash': (_) => const SplashScreen(),
-        '/onboarding': (_) => const OnboardingScreen(),
-        '/login': (_) => const LoginScreen(),
-        '/register': (_) => const RegisterScreen(),
-        '/home': (_) => const BottomNavigationScreen(),
-      },
-    );
-  }
-}
+// void main() {
+//   runApp(const MyApp());
+// }
+
+// Future<void> main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await HiveService.init();
+//   runApp(const App());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       initialRoute: '/splash',
+//       routes: {
+//         '/splash': (_) => const SplashScreen(),
+//         '/onboarding': (_) => const OnboardingScreen(),
+//         '/login': (_) => const LoginScreen(),
+//         '/register': (_) => const RegisterScreen(),
+//         '/home': (_) => const BottomNavigationScreen(),
+//       },
+//     );
+//   }
+// }
