58ea196b2ffdc333eddbb10433e725ccdbc63cfa
Samekshya <samikshyabaniya003@gmail.com>
2026-01-01 20:50:27 +0545

    sprint 3 completed

--- lib/screens/bottom_screen/bottomnavigation_screen.dart
+++ lib/screens/bottom_screen/bottomnavigation_screen.dart
@@ -1,18 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../../features/auth/presentation/providers/auth_providers.dart';
 import 'dashboard_tab.dart';
 import 'explore_tab.dart';
 import 'saved_tab.dart';
 import 'profile_tab.dart';
 
-class BottomNavigationScreen extends StatefulWidget {
+class BottomNavigationScreen extends ConsumerStatefulWidget {
   const BottomNavigationScreen({super.key});
 
   @override
-  State<BottomNavigationScreen> createState() => _BottomNavigationScreenState();
+  ConsumerState<BottomNavigationScreen> createState() =>
+      _BottomNavigationScreenState();
 }
 
-class _BottomNavigationScreenState extends State<BottomNavigationScreen> {
+class _BottomNavigationScreenState
+    extends ConsumerState<BottomNavigationScreen> {
   int _selectedIndex = 0;
 
   final List<Widget> _pages = const [
@@ -25,6 +29,18 @@ class _BottomNavigationScreenState extends State<BottomNavigationScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Trekly'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.logout),
+            onPressed: () {
+              ref.read(authRepositoryProvider).logout();
+              Navigator.pushReplacementNamed(context, '/login');
+            },
+          ),
+        ],
+      ),
       body: _pages[_selectedIndex],
       bottomNavigationBar: BottomNavigationBar(
         type: BottomNavigationBarType.fixed,
