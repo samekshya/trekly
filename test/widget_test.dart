c2f790ad0bd57d8e232188881560291909c63ae2
Samekshya <samikshyabaniya003@gmail.com>
2026-01-31 17:28:23 +0545

    add test for app bar title

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -41,13 +41,3 @@ void main() {
 //     expect(find.text('1'), findsOneWidget);
 //   });
 // }
-//     // Verify that our counter has incremented.
-//     expect(find.text('0'), findsNothing);
-//     expect(find.text('1'), findsOneWidget);
-//   });
-// }
-//     // Verify that our counter has incremented.
-//     expect(find.text('0'), findsNothing);
-//     expect(find.text('1'), findsOneWidget);
-//   });
-// }
