b3520e43cd8763690f99d77958bf3ac81d18bf6e
Samekshya <samikshyabaniya003@gmail.com>
2026-03-09 21:15:01 +0545

    toeken storage updated

--- lib/core/services/storage/token_storage.dart
+++ lib/core/services/storage/token_storage.dart
@@ -1,21 +1,39 @@
 import 'package:hive/hive.dart';
 
 class TokenStorage {
-  static const _box = "authBox";
-  static const _key = "token";
+  static const _boxName = 'authBox';
+  static const _tokenKey = 'token';
+  static const _userKey = 'user';
 
-  Future<void> save(String token) async {
-    final box = await Hive.openBox(_box);
-    await box.put(_key, token);
+  Future<void> saveToken(String token) async {
+    final box = await Hive.openBox(_boxName);
+    await box.put(_tokenKey, token);
   }
 
-  Future<String?> read() async {
-    final box = await Hive.openBox(_box);
-    return box.get(_key) as String?;
+  Future<String?> getToken() async {
+    final box = await Hive.openBox(_boxName);
+    return box.get(_tokenKey) as String?;
+  }
+
+  Future<void> saveUser(Map<String, dynamic> user) async {
+    final box = await Hive.openBox(_boxName);
+    await box.put(_userKey, user);
+  }
+
+  Future<Map<String, dynamic>?> getUser() async {
+    final box = await Hive.openBox(_boxName);
+    final data = box.get(_userKey);
+    if (data == null) return null;
+    return Map<String, dynamic>.from(data as Map);
+  }
+
+  Future<bool> hasToken() async {
+    final token = await getToken();
+    return token != null && token.isNotEmpty;
   }
 
   Future<void> clear() async {
-    final box = await Hive.openBox(_box);
-    await box.delete(_key);
+    final box = await Hive.openBox(_boxName);
+    await box.clear();
   }
 }
